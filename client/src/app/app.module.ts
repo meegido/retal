c6883cf98b6613e8fc210e1f2412bdfcf0e01419
Mercedes Egido <meegido@gmail.com>
2017-10-03 17:00:59 +0200

    front auth services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {routes} from './routes';
+import { AuthService } from './services/auth.service';
+import { IsLoggedInService } from './services/is-logged-in.canactivate.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService, IsLoggedInService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+import {IsLoggedInService} from './services/is-logged-in.canactivate.service';
+
+export const routes: Routes = [ ];

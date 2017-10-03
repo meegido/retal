0fede67ec2d143134aaa5f4c4e2242720aa6a7a9
Mercedes Egido <meegido@gmail.com>
2017-10-03 21:42:48 +0200

    login component working

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { SignupformComponent } from './signupform/signupform.component'
+import { SignupformComponent } from './signupform/signupform.component';
+import {LoginformComponent} from './loginform/loginform.component';
 
 import {IsLoggedInService} from './services/is-logged-in.canactivate.service';
 
 export const routes: Routes = [
   //{ path: '', component: HomeComponent },
   //{ path: 'user',  component: UserprofileComponent,canActivate: [ IsLoggedInService ]  },
-  //{ path: 'login',  component: LoginformComponent,  },
+  { path: 'login',  component: LoginformComponent,  },
   { path: 'signup',  component: SignupformComponent,  },
   { path: '**', redirectTo: '' }
 ];

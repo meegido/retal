38996fc4a38ea9664176ee792f2014f8b245fbb0
Mercedes Egido <meegido@gmail.com>
2017-10-05 16:19:43 +0200

    campaigns service

---
+++ client/src/app/services/campaign.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class CampaignService {
+
+  BASE_URL: string = 'http://localhost:3000';
+    constructor(private http: Http) {}
+
+}
+
+getList() {
+  return this.http.get(`${this.BASE_URL}/api/campaigns`)
+  .map((res) => res.json());
+}

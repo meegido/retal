3388dd9221fc58157be0be450cdd4a1a04ee24ec
Mercedes Egido <meegido@gmail.com>
2017-10-09 13:41:43 +0200

    images on list campaign

--- server/routes/api/order/Order.js
+++ server/routes/api/order/Order.js
@@ -5,6 +5,8 @@ const orderSchema = new Schema({
     'meters': Number,
     'maker': { type: Schema.Types.ObjectId, ref: 'User' },
     'buyer': [{ type: Schema.Types.ObjectId, ref: 'User'}]
+    // buyerName: String,
+    // buyerMail: String
 })
 orderSchema.set('timestamps', true);
 
